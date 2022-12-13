c9c57f2f6fab19613417ee8f5ade4b9a33ea59bb
Thomas Gehrmann <s6thgehr@gmail.com>
2022-12-13 18:59:16 +0100

    Create fake NFT data

--- src/components/RequestAirdrop.tsx
+++ src/components/RequestAirdrop.tsx
@@ -53,7 +53,7 @@ export const RequestAirdrop: FC = () => {
     return (
         <div>
             <button
-                className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+                className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#4a2103] to-[#e4f009] hover:from-pink-500 hover:to-yellow-500 ..."
                 onClick={onClick}
             >
                 <span>Airdrop 1 </span>
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,15 @@
 // Next, React
 import { FC, useEffect, useState } from "react";
 import Link from "next/link";
+import {
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList,
+    Divider,
+} from "@chakra-ui/react";
+import { GiChest } from "react-icons/gi";
 
 // Wallet
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
@@ -19,6 +28,31 @@ export const HomeView: FC = ({}) => {
     const balance = useUserSOLBalanceStore((s) => s.balance);
     const { getUserSOLBalance } = useUserSOLBalanceStore();
 
+    const fakeNftData = [
+        {
+            name: "SMB#321",
+            isChest: false,
+        },
+        {
+            name: "y00ts#71",
+            isChest: false,
+        },
+        {
+            name: "DeGods#198",
+            isChest: true,
+        },
+        {
+            name: "Claynosaurz#22",
+            isChest: false,
+        },
+    ];
+
+    const [nftData, setNftData] = useState([]);
+
+    useEffect(() => {
+        setNftData(fakeNftData);
+    }, []);
+
     useEffect(() => {
         if (wallet.publicKey) {
             console.log(wallet.publicKey.toBase58());
@@ -29,23 +63,23 @@ export const HomeView: FC = ({}) => {
     return (
         <div className="md:hero mx-auto p-4">
             <div className="md:hero-content flex flex-col">
-                <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-                    Scaffold Lite{" "}
-                    <span className="text-sm font-normal align-top text-slate-700">
+                <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#4a2103] to-[#e4f009]">
+                    Solana Chest
+                    {/* <span className="text-sm font-normal align-top text-slate-700">
                         v{pkg.version}
-                    </span>
+                    </span> */}
                 </h1>
                 <h4 className="md:w-full text-center text-slate-300 my-2">
-                    <p>Simply the fastest way to get started.</p>
-                    Next.js, tailwind, wallet, web3.js, and more.
+                    Transform your Token into a chest which can store other SPL
+                    tokens.
                 </h4>
-                <div className="max-w-md mx-auto mockup-code bg-primary p-6 my-2">
+                {/* <div className="max-w-md mx-auto mockup-code bg-primary p-6 my-2">
                     <pre data-prefix=">">
                         <code className="truncate">
                             Start building on Solana{" "}
                         </code>
                     </pre>
-                </div>
+                </div> */}
                 <div className="text-center">
                     <RequestAirdrop />
                     {/* {wallet.publicKey && <p>Public Key: {wallet.publicKey.toBase58()}</p>} */}
@@ -53,6 +87,37 @@ export const HomeView: FC = ({}) => {
                         <p>SOL Balance: {(balance || 0).toLocaleString()}</p>
                     )}
                 </div>
+                {wallet.connected && (
+                    <>
+                        <h2 className="text-center text-3xl md:pl-12 bg-clip-text">
+                            Your tokens
+                        </h2>
+                        <List spacing={2}>
+                            {nftData.map((item, index) => {
+                                return (
+                                    <ListItem key={index}>
+                                        <div className="flex items-stretch">
+                                            <div className="mr-16">
+                                                {item.name}{" "}
+                                            </div>
+                                            {item.isChest ? (
+                                                <ListIcon
+                                                    as={GiChest}
+                                                    color="green.500"
+                                                    className="ml-auto"
+                                                />
+                                            ) : (
+                                                <button className="ml-auto">
+                                                    Transform to chest
+                                                </button>
+                                            )}
+                                        </div>
+                                    </ListItem>
+                                );
+                            })}
+                        </List>
+                    </>
+                )}
             </div>
         </div>
     );
